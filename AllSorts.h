dc816613387b8b850026cc26183d24ec9b0ea991
fanqiliang <fanqiliang1@126.com>
2013-12-26 00:34:08 +0800

    change

--- AllSorts.h
+++ AllSorts.h
@@ -8,4 +8,11 @@ extern void InsertSort(int Array[], int low, int high);
 
 extern int  Partition(int Array[], int low, int high);
 extern void QuickSort(int Array[], int low, int high);
+
+extern void ShellSort(int Array[], int low, int high);
+
+extern void MergeSort(int Array[], int low, int high);
+extern void Merge(int Array[], int low, int mid, int high);
+
+extern void BucketSort(int Array[], int low, int high);
 #endif
